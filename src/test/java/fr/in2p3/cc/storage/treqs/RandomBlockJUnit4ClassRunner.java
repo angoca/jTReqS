e645d82b31d049e3dbaaaed44908662d36f7e27b
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-08-20 17:11:47 +0200

    Random tests

---
+++ src/test/java/fr/in2p3/cc/storage/treqs/RandomBlockJUnit4ClassRunner.java
@@ -0,0 +1,29 @@
+package fr.in2p3.cc.storage.treqs;
+
+import java.util.Collections;
+
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.InitializationError;
+
+/**
+ * Taken from
+ * http://stackoverflow.com/questions/1444314/how-can-i-make-my-junit-
+ * tests-run-in-random-order
+ *
+ * @author Stackioverflow
+ */
+public class RandomBlockJUnit4ClassRunner extends BlockJUnit4ClassRunner {
+
+    public RandomBlockJUnit4ClassRunner(Class<?> klass)
+            throws InitializationError {
+        super(klass);
+    }
+
+    protected java.util.List<org.junit.runners.model.FrameworkMethod> computeTestMethods() {
+        java.util.List<org.junit.runners.model.FrameworkMethod> methods = super
+                .computeTestMethods();
+        Collections.shuffle(methods);
+        return methods;
+    }
+
+}
