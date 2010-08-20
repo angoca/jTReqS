ad92977012b166183ae3f3ed977ebbf1e55dd8ee
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-08-20 18:03:41 +0200

    directories and parameters

--- compile.sh
+++ compile.sh
@@ -1,9 +1,10 @@
 echo LD_LIBRARY
 export LD_LIBRARY_PATH=/opt/hpss/lib:`pwd`/bin
 echo javah
-javah -classpath bin -d src/main/c/ -jni fr.in2p3.cc.storage.treqs.hsm.hpss.HPSSBridge
+javah -verbose -classpath bin -d bin -jni fr.in2p3.cc.storage.treqs.hsm.hpssJNI.HPSSBridge
+#javah -verbose -classpath bin -d target/classes/fr/in2p3/cc/storage/treqs/hsm/hpssJNI/ -jni  fr.in2p3.cc.storage.treqs.hsm.hpssJNI.HPSSBridge
 echo gcc
-gcc -I /opt/jdk1.6.0_18/include/linux/ -I /opt/hpss/include -o bin/libHPSSBridge.so -fPIC -DLINUX src/main/c/HPSSBridge.c -lhpss -L/opt/hpss/lib -shared
+gcc -I /opt/jdk1.6.0_18/include/linux/ -I /opt/hpss/include -I bin -o bin/libHPSSBridge.so -fPIC -DLINUX src/main/c/HPSSBridge.c -lhpss -L/opt/hpss/lib -shared
 echo java
 #java -Djava.library.path=/opt/hpss/lib/:bin -cp bin:../../db2sa/vendor/org/slf4j/slf4j-api/1.6.1/slf4j-api-1.6.1.jar:../../db2sa/vendor/ch/qos/logback/logback-core/0.9.24/logback-core-0.9.24.jar:../../db2sa/vendor/ch/qos/logback/logback-classic/0.9.24/logback-classic-0.9.24.jar fr.in2p3.cc.storage.treqs.hsm.hpss.HPSSBridge
 
@@ -16,6 +17,6 @@ slf4j=${libraries}/slf4j/slf4j-1.6.1/slf4j-api-1.6.1.jar
 logbackCore=${libraries}/logback/logback-0.9.24/logback-core-0.9.24.jar
 logbackClassic=${libraries}/logback/logback-0.9.24/logback-classic-0.9.24.jar
 
-java="java -Djava.library.path=/opt/hpss/lib/:bin -cp bin:${commonsCli}:${mysql}:${commonsCollections}:${slf4j}:${logbackCore}:${logbackClassic} -Dlogback.configurationFile=logback-test.xml fr.in2p3.cc.storage.treqs.main.Main"
+java="java -Djava.library.path=/opt/hpss/lib/:bin -cp bin:${commonsCli}:${mysql}:${commonsCollections}:${slf4j}:${logbackCore}:${logbackClassic} -Dlogback.configurationFile=bin/logback-test.xml fr.in2p3.cc.storage.treqs.hsm.hpssJNI.HPSSBridge /afs/in2p3.fr/home/g/gomez/keytab.treqs /hpss"
 echo ${java}
 ${java}
