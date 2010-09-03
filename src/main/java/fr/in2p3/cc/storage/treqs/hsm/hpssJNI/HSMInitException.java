0a63451c8829cc0b091edd78a16d59fe992e809a
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-09-03 18:32:24 +0200

    Exception throw when hsm init

---
+++ src/main/java/fr/in2p3/cc/storage/treqs/hsm/hpssJNI/HSMInitException.java
@@ -0,0 +1,12 @@
+package fr.in2p3.cc.storage.treqs.hsm.hpssJNI;
+
+import fr.in2p3.cc.storage.treqs.hsm.exception.HSMException;
+
+public class HSMInitException extends HSMException {
+
+    /**
+     * Generated ID.
+     */
+    private static final long serialVersionUID = -7896315209618198341L;
+
+}
