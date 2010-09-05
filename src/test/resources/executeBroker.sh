b4fde586327e71b0c641dfc3b77bfd01b7611490
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-09-06 00:28:37 +0200

    Scripts for each test

--- src/test/resources/executeBroker.sh
+++ src/test/resources/executeBroker.sh
@@ -1,6 +1,16 @@
+# Calls the HPSS Broker compiler
+sh ./compileBroker.sh
+
 # HPSS Broker test
-# Library
-ld -o ./libHPSSBroker.so ./HPSSBroker.o -shared
-#-lhpss -L/opt/hpss/lib
-# Executable
-gcc -I /opt/hpss/include -I ./ -DLINUX -lHPSSBroker -L./ -o ./BrokerTester ../src/test/c/HPSSBrokerTester.c -lhpss -L /opt/hpss/lib/ -pthread
+# Create the library
+echo Creating library
+ld -shared -o ./libHPSSBroker.so ./HPSSBroker.o
+# Compile the executable
+echo Compiling executable
+gcc -I /opt/hpss/include -I ./ -lhpss -L /opt/hpss/lib/ -lHPSSBroker -L./ -DLINUX -pthread -o ./brokerTester ../src/test/c/HPSSBrokerTester.c
+
+
+# Execute.
+echo Executing
+export HPSS_API_DEBUG=255
+./brokerTester
