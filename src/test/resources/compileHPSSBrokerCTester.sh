f36eafe0814b0e35b7fe9f78764517291c85000c
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-09-06 00:27:04 +0200

    Renamed

--- src/test/resources/compileHPSSBrokerCTester.sh
+++
@@ -1,2 +0,0 @@
-gcc -I /opt/hpss/include/ -I ./ -DLINUX -lHPSSJNIBridge -L./ -o ./testC ../src/test/c/HPSSBrokerTester.c -lhpss -L /opt/hpss/lib/ -pthread
-
--- src/test/resources/compileHPSSBrokerJavaTester.sh
+++
@@ -1,4 +0,0 @@
-./compileHPSSBrokerTest.sh
-
-java -Djava.library.path=classes -cp bin fr.in2p3.cc.storage.treqs.hsm.hpssJNI.HPSSJNIBridge
-
