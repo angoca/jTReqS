cc6f0fb741a5601a8442b8668845edf685dde2ff
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-11-03 18:26:36 +0100

    Compilation de java before

--- src/test/resources/executeJNIBridgeJava.sh
+++ src/test/resources/executeJNIBridgeJava.sh
@@ -2,5 +2,6 @@
 sh ./compileJNIBridge.sh
 
 # HPSS JNI Bridge in Java.
+javac -cp ../vendor/slf4j/slf4j-1.6.1/slf4j-api-1.6.1.jar:. -d . ../src/test/java/fr/in2p3/cc/storage/treqs/hsm/hpssJNI/HPSSJNIBridgeTester.java
 java -Djava.library.path=./ -cp ../vendor/slf4j/slf4j-1.6.1/slf4j-api-1.6.1.jar:../vendor/logback/logback-0.9.24/logback-core-0.9.24.jar:../vendor/logback/logback-0.9.24/logback-classic-0.9.24.jar:./ fr.in2p3.cc.storage.treqs.hsm.hpssJNI.HPSSJNIBridgeTester
 
