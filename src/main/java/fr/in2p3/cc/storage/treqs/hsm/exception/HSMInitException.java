8942995463e1d0ef5bfd33a2e361d4ca0e3dadf1
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-11-03 18:25:48 +0100

    new constructor

--- src/main/java/fr/in2p3/cc/storage/treqs/hsm/exception/HSMInitException.java
+++ src/main/java/fr/in2p3/cc/storage/treqs/hsm/exception/HSMInitException.java
@@ -1,6 +1,9 @@
 package fr.in2p3.cc.storage.treqs.hsm.exception;
 
 public class HSMInitException extends HSMException {
+    public HSMInitException(String errorCode) {
+        this(Integer.parseInt(errorCode));
+    }
     public HSMInitException(int errorCode) {
         super("" + errorCode);
     }
