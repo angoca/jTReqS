5acd61fafd2980f141cb242ea1a162f541d12722
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-11-18 15:18:02 +0100

    DAO queue reviewed

--- src/main/java/fr/in2p3/cc/storage/treqs/persistance/mysql/dao/MySQLQueueDAO.java
+++ src/main/java/fr/in2p3/cc/storage/treqs/persistance/mysql/dao/MySQLQueueDAO.java
@@ -1,9 +1,7 @@
-package fr.in2p3.cc.storage.treqs.persistance.mysql.dao;
-
 /*
  * Copyright      Jonathan Schaeffer 2009-2010,
  *                  CC-IN2P3, CNRS <jonathan.schaeffer@cc.in2p3.fr>
- * Contributors : Andres Gomez,
+ * Contributors   Andres Gomez,
  *                  CC-IN2P3, CNRS <andres.gomez@cc.in2p3.fr>
  *
  * This software is a computer program whose purpose is to schedule, sort
@@ -36,6 +34,7 @@ package fr.in2p3.cc.storage.treqs.persistance.mysql.dao;
  * knowledge of the CeCILL license and that you accept its terms.
  *
  */
+package fr.in2p3.cc.storage.treqs.persistance.mysql.dao;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -45,68 +44,43 @@ import java.util.Calendar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.in2p3.cc.storage.treqs.model.Constants;
+import fr.in2p3.cc.storage.treqs.model.Queue;
 import fr.in2p3.cc.storage.treqs.model.QueueStatus;
-import fr.in2p3.cc.storage.treqs.model.Tape;
 import fr.in2p3.cc.storage.treqs.model.dao.QueueDAO;
-import fr.in2p3.cc.storage.treqs.persistance.PersistanceException;
+import fr.in2p3.cc.storage.treqs.model.exception.TReqSException;
 import fr.in2p3.cc.storage.treqs.persistance.mysql.MySQLBroker;
 import fr.in2p3.cc.storage.treqs.persistance.mysql.MySQLStatements;
 import fr.in2p3.cc.storage.treqs.persistance.mysql.exception.ExecuteMySQLException;
 
 /**
- * Managing Queues object updates to database
+ * Manage the Queues object insert and updates to MySQL database.
+ *
+ * @author Jonathan Schaeffer
+ * @since 1.0
  */
 public class MySQLQueueDAO implements QueueDAO {
 
-    /**
-     * Singleton initialization
-     */
-    private static MySQLQueueDAO _instance = null;
     /**
      * Logger.
      */
     private static final Logger LOGGER = LoggerFactory
             .getLogger(MySQLQueueDAO.class);
 
-    /**
-     * Destroys the only instance. ONLY for testing purposes.
-     */
-    public static void destroyInstance() {
-        LOGGER.trace("> destroyInstance");
-
-        _instance = null;
-
-        LOGGER.trace("< destroyInstance");
-    }
-
-    /**
-     * @return
-     */
-    public static QueueDAO getInstance() {
-        LOGGER.trace("> getInstance");
-
-        if (_instance == null) {
-            LOGGER.debug("Creating singleton");
-            _instance = new MySQLQueueDAO();
-        }
-
-        LOGGER.trace("< getInstance");
-
-        return _instance;
-    }
-
     /*
      * (non-Javadoc)
-     * 
      * @see fr.in2p3.cc.storage.treqs.model.dao.QueueDAO#abortPendingQueues()
      */
-    public int abortPendingQueues() throws PersistanceException {
+    @Override
+    public final int abortPendingQueues() throws TReqSException {
         LOGGER.trace("> abortPendingQueues");
 
         LOGGER.info("Cleaning unfinished queues");
 
         int ret = MySQLBroker.getInstance().executeModification(
-                MySQLStatements.SQL_UPDATE_QUEUES_ON_STARTUP);
+                MySQLStatements.SQL_QUEUES_UPDATE_ABORT_ON_STARTUP);
+
+        assert ret >= 0;
 
         LOGGER.trace("< abortPendingQueues");
 
@@ -115,42 +89,43 @@ public class MySQLQueueDAO implements QueueDAO {
 
     /*
      * (non-Javadoc)
-     * 
      * @see
      * fr.in2p3.cc.storage.treqs.model.dao.QueueDAO#insert(fr.in2p3.cc.storage
-     * .treqs.model.QueueStatus, fr.in2p3.cc.storage.treqs.model.Tape, int,
-     * long, java.util.Calendar)
+     * .treqs.model.Queue)
      */
-    // @Override
-    public int insert(QueueStatus status, Tape tape, int size, long byteSize,
-            Calendar creationTime) throws ExecuteMySQLException {
+    @Override
+    public final int insert(final Queue queue) throws TReqSException {
         LOGGER.trace("> insert");
 
-        assert status != null;
-        assert tape != null;
-        assert size >= 0;
-        assert byteSize >= 0;
-        assert creationTime != null;
+        assert queue != null;
+
+        final int statusId = queue.getStatus().getId();
+        final String tapeName = queue.getTape().getName();
+        final int size = queue.getRequestsSize();
+        final byte mediaTypeId = queue.getTape().getMediaType().getId();
+        final long byteSize = queue.getByteSize();
+        final long creationTimeMillis = queue.getCreationTime()
+                .getTimeInMillis();
 
         int id = 0;
         PreparedStatement statement = MySQLBroker.getInstance()
-                .getPreparedStatement(MySQLStatements.SQL_INSERT_QUEUE);
+                .getPreparedStatement(MySQLStatements.SQL_QUEUES_INSERT_QUEUE);
         try {
             int index = 1;
-            // Insert queue Status
-            statement.setInt(index++, status.getId());
-            // Insert the name
-            statement.setString(index++, tape.getName());
-            // insert nbjobs
+            // Insert queue status.
+            statement.setInt(index++, statusId);
+            // Insert name.
+            statement.setString(index++, tapeName);
+            // Insert number of jobs.
             statement.setInt(index++, size);
-            // insert pvrid
-            statement.setByte(index++, tape.getMediaType().getId());
-            // insert owner
+            // Insert media type id.
+            statement.setByte(index++, mediaTypeId);
+            // Insert owner.
             statement.setString(index++, "");
-            // insert size
+            // Insert size.
             statement.setLong(index++, byteSize);
-            // insert time
-            statement.setLong(index++, creationTime.getTimeInMillis());
+            // Insert time.
+            statement.setLong(index++, creationTimeMillis);
 
             statement.execute();
 
@@ -167,6 +142,8 @@ public class MySQLQueueDAO implements QueueDAO {
         }
         LOGGER.info("New queue inserted with id " + id);
 
+        assert id >= 0;
+
         LOGGER.trace("< insert");
 
         return id;
@@ -174,34 +151,37 @@ public class MySQLQueueDAO implements QueueDAO {
 
     /*
      * (non-Javadoc)
-     * 
-     * @see fr.in2p3.cc.storage.treqs.model.dao.QueueDAO#updateAddRequest(int,
-     * java.lang.String, long, int)
+     * @see
+     * fr.in2p3.cc.storage.treqs.model.dao.QueueDAO#updateAddRequest(fr.in2p3
+     * .cc.storage.treqs.model.Queue)
      */
-    // @Override
-    public void updateAddRequest(int jobsSize, String ownerName, long byteSize,
-            int id) throws ExecuteMySQLException {
+    @Override
+    public final void updateAddRequest(final Queue queue) throws TReqSException {
         LOGGER.trace("> updateAddRequest");
 
-        assert jobsSize > 0;
-        assert ownerName != null;
-        assert !ownerName.equals("");
-        assert byteSize > 0;
-        assert id >= 0;
+        assert queue != null;
+
+        String ownerName = Constants.NO_OWNER_NAME;
+        if (queue.getOwner() != null) {
+            ownerName = queue.getOwner().getName();
+        }
+        final int id = queue.getId();
+        final int size = queue.getRequestsSize();
+        final long byteSize = queue.getByteSize();
 
         PreparedStatement statement = MySQLBroker.getInstance()
                 .getPreparedStatement(
-                        MySQLStatements.SQL_UPDATE_QUEUE_ADD_REQUEST);
+                        MySQLStatements.SQL_QUEUES_UPDATE_ADD_REQUEST);
 
         int index = 1;
-        // Insert number of jobs
         try {
-            statement.setInt(index++, jobsSize);
-            // insert owner
+            // Insert number of requests.
+            statement.setInt(index++, size);
+            // Insert owner.
             statement.setString(index++, ownerName);
-            // insert size
+            // Insert size.
             statement.setLong(index++, byteSize);
-            // insert Id
+            // Insert Id.
             statement.setInt(index++, id);
 
             statement.execute();
@@ -221,71 +201,107 @@ public class MySQLQueueDAO implements QueueDAO {
 
     /*
      * (non-Javadoc)
-     * 
      * @see
-     * fr.in2p3.cc.storage.treqs.model.dao.QueueDAO#updateState(java.util.Calendar
-     * , fr.in2p3.cc.storage.treqs.model.QueueStatus, int, short, short,
-     * java.lang.String, long, int)
+     * fr.in2p3.cc.storage.treqs.model.dao.QueueDAO#updateState(fr.in2p3.cc.
+     * storage.treqs.model.Queue, java.util.Calendar, short, short)
      */
-    // @Override
-    public void updateState(Calendar time, QueueStatus status, int size,
-            short nbDone, short nbFailed, String ownerName, long byteSize,
-            int id) throws ExecuteMySQLException {
+    @Override
+    public final void updateState(final Queue queue, final Calendar time,
+            final short nbDone, final short nbFailed) throws TReqSException {
         LOGGER.trace("> updateState");
 
+        assert queue != null;
         assert time != null;
-        assert status != null;
-        assert size >= 0;
         assert nbDone >= 0;
         assert nbFailed >= 0;
-        assert ownerName != null;
-        assert byteSize >= 0;
-        assert id >= 0;
+
+        final QueueStatus status = queue.getStatus();
 
         PreparedStatement statement = null;
         int index = 1;
 
         try {
             switch (status) {
-            case QS_ACTIVATED:
-                statement = MySQLBroker.getInstance().getPreparedStatement(
-                        MySQLStatements.SQL_UPDATE_QUEUE_ACTIVATED);
-                // insert activation time
-                statement.setLong(index++, time.getTimeInMillis());
-                break;
-            case QS_CREATED:
-                statement = MySQLBroker.getInstance().getPreparedStatement(
-                        MySQLStatements.SQL_UPDATE_QUEUE_UNSUSPENDED);
-                break;
-            case QS_ENDED:
-                // This should be QS_ENDED or QS_ABORTED or
-                // QS_TEMPORALLY_SUSPENDED
-                statement = MySQLBroker.getInstance().getPreparedStatement(
-                        MySQLStatements.SQL_UPDATE_QUEUE_ENDED);
-                // insert end time
-                statement.setLong(index++, time.getTimeInMillis());
-                break;
-            default:
-                assert false;
+                case QS_ACTIVATED:
+                    statement = MySQLBroker.getInstance().getPreparedStatement(
+                            MySQLStatements.SQL_QUEUES_UPDATE_QUEUE_ACTIVATED);
+                    // Insert activation time
+                    statement.setLong(index++, time.getTimeInMillis());
+                    break;
+                case QS_CREATED:
+                    // This call could be done when the queue is unsuspended.
+                    LOGGER.error("This is an invalid state call.");
+                    assert false;
+                    break;
+                case QS_ENDED:
+                    // This should be QS_ENDED or QS_ABORTED or
+                    // QS_TEMPORALLY_SUSPENDED
+                    statement = MySQLBroker.getInstance().getPreparedStatement(
+                            MySQLStatements.SQL_QUEUES_UPDATE_QUEUE_ENDED);
+                    // Insert end time.
+                    statement.setLong(index++, time.getTimeInMillis());
+                    break;
+                default:
+                    assert false;
             }
         } catch (SQLException e) {
             throw new ExecuteMySQLException(e);
         }
 
-        // Insert queue Status
+        processUpdate(queue, nbDone, nbFailed, statement, index);
+
+        LOGGER.trace("< updateState");
+    }
+
+    /**
+     * Fills the statement and execute it.
+     *
+     * @param queue
+     *            Queue to update.
+     * @param nbDone
+     *            Number of requests done.
+     * @param nbFailed
+     *            Number of failed requests.
+     * @param statement
+     *            Statement to fill and execute.
+     * @param i
+     *            Index in the statement.
+     * @throws ExecuteMySQLException
+     *             If there is a problem executing the query.
+     */
+    private void processUpdate(final Queue queue, final short nbDone,
+            final short nbFailed, final PreparedStatement statement, final int i)
+            throws ExecuteMySQLException {
+        LOGGER.trace("> processUpdate");
+
+        assert queue != null;
+        assert nbDone >= 0;
+        assert nbFailed >= 0;
+        assert statement != null;
+        assert i > 0;
+
+        final int id = queue.getId();
+        final int statusId = queue.getStatus().getId();
+        final String ownerName = queue.getOwner().getName();
+        final int size = queue.getRequestsSize();
+        final long byteSize = queue.getByteSize();
+
+        int index = i;
+
         try {
-            statement.setInt(index++, status.getId());
-            // insert nbjobs
+            // Insert queue status.
+            statement.setInt(index++, statusId);
+            // Insert number of requests.
             statement.setInt(index++, size);
-            // insert nbdone
+            // Insert number of requests done.
             statement.setInt(index++, nbDone);
-            // insert nbfailed
+            // Insert number of requests failed.
             statement.setInt(index++, nbFailed);
-            // insert owner
+            // Insert owner.
             statement.setString(index++, ownerName);
-            // insert size
+            // Insert size.
             statement.setLong(index++, byteSize);
-            // insert Id
+            // Insert Id.
             statement.setInt(index++, id);
 
             statement.execute();
@@ -302,6 +318,6 @@ public class MySQLQueueDAO implements QueueDAO {
             }
         }
 
-        LOGGER.trace("< updateState");
+        LOGGER.trace("< processUpdate");
     }
 }
