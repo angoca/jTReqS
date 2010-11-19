3a0524892edc3158c6041b78e55b33e5052dbd33
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-11-19 16:09:12 +0100

    Cofigurator reiewed

--- src/main/java/fr/in2p3/cc/storage/treqs/tools/Configurator.java
+++ src/main/java/fr/in2p3/cc/storage/treqs/tools/Configurator.java
@@ -1,9 +1,7 @@
-package fr.in2p3.cc.storage.treqs.tools;
-
 /*
  * Copyright      Jonathan Schaeffer 2009-2010,
  *                  CC-IN2P3, CNRS <jonathan.schaeffer@cc.in2p3.fr>
- * Contributors : Andres Gomez,
+ * Contributors   Andres Gomez,
  *                  CC-IN2P3, CNRS <andres.gomez@cc.in2p3.fr>
  *
  * This software is a computer program whose purpose is to schedule, sort
@@ -36,90 +34,102 @@ package fr.in2p3.cc.storage.treqs.tools;
  * knowledge of the CeCILL license and that you accept its terms.
  *
  */
+package fr.in2p3.cc.storage.treqs.tools;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.InvalidPropertiesFormatException;
-import java.util.Iterator;
 import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fr.in2p3.cc.storage.treqs.model.exception.ConfigNotFoundException;
-import fr.in2p3.cc.storage.treqs.model.exception.ProblematicConfiguationFileException;
+import fr.in2p3.cc.storage.treqs.model.DefaultProperties;
 
-public class Configurator {
+/**
+ * Reads the configuration from the properties file and then, keep that
+ * information in memory.
+ *
+ * @author Jonathan Schaeffer
+ * @since 1.0
+ */
+public final class Configurator {
 
     /**
      * Singleton initialization.
      */
-    private static Configurator _instance = null;
+    private static Configurator instance = null;
     /**
      * Logger.
      */
     private static final Logger LOGGER = LoggerFactory
             .getLogger(Configurator.class);
 
+    /**
+     * Destroy the instance. Use for testing purposes.
+     */
     public static void destroyInstance() {
         LOGGER.trace("> destroyInstance");
 
-        _instance = null;
+        instance = null;
 
         LOGGER.trace("< destroyInstance");
     }
 
     /**
-     * Singleton access
-     * 
-     * @return
+     * Singleton access.
+     *
+     * @return the unique instance of the configurator.
      */
     public static Configurator getInstance() {
         LOGGER.trace("> getInstance");
 
-        if (_instance == null)
-            _instance = new Configurator();
+        if (instance == null) {
+            instance = new Configurator();
+        }
+
+        assert instance != null;
 
         LOGGER.trace("< getInstance");
 
-        return _instance;
+        return instance;
     }
 
     /**
      * Path to the configuration file.
      */
-    private String confFilename;
+    private String configFilename;
 
     /**
      * The map containing the configuration.
      */
     private Properties properties;
 
+    /**
+     * Constructor of the configurator where it defines the name of the
+     * configuration file.
+     */
     private Configurator() {
-        LOGGER.trace("> TReqSConfig");
+        LOGGER.trace("> Create instance");
 
-        this.confFilename = "treqs.conf.properties";
+        this.configFilename = DefaultProperties.CONFIGURATION_PROPERTIES;
 
-        LOGGER.trace("< TReqSConfig");
+        LOGGER.trace("< Create instance");
     }
 
     /**
-     * Deletes value, For testing purposes.
-     * 
+     * Deletes a value from the properties. For testing purposes.
+     *
      * @param sec
+     *            Section.
      * @param key
+     *            Name of the property.
      */
-    public void deleteValue(String sec, String key) {
+    public void deleteValue(final String sec, final String key) {
         LOGGER.trace("> deleteValue");
 
-        assert sec != null;
-        assert !sec.equals("");
-        assert key != null;
-        assert !key.equals("");
+        assert sec != null && !sec.equals("");
+        assert key != null && !key.equals("");
 
         if (this.properties != null) {
             this.properties.remove(sec + "." + key);
@@ -129,64 +139,50 @@ public class Configurator {
     }
 
     /**
-     * Computes the configuration file Order is :
+     * Reads the configuration file indicated in the configFilename attribute,
+     * and then put the configuration in the properties attribute.
      * <p>
-     * <ol>
-     * <li>command line "jtreqsConfigFile" parameter</li>
-     * <li>$HOME/.treqs.conf</li>
-     * <li>$TREQSC_HOME/etc/treqs.conf.properties</li>
-     * <li>/etc/treqs.conf.properties</li>
-     * </ol>
-     * <p>
-     * If found nowhere, don't try to continue, it's hopeless. Throw an
-     * exception
-     * 
+     * This method was taken from Zemucansuka:FileReader.java.
+     *
      * @throws ProblematicConfiguationFileException
      *             If there is a problem reading the file.
      */
     private void findConfigPath() throws ProblematicConfiguationFileException {
         LOGGER.trace("> findConfigPath");
 
-        ArrayList<String> names = new ArrayList<String>();
+        assert this.configFilename != null;
 
-        if (this.confFilename != "") {
-            names.add(this.confFilename);
-            LOGGER.debug("Adding " + names.get(names.size() - 1));
-        }
-        names.add("/etc/treqs.conf.properties");
-        LOGGER.debug("Adding " + names.get(names.size() - 1));
-
-        Properties props = new Properties();
-        boolean found = false;
-        // Try to open this->Configpath for reading
-        for (Iterator<String> iterator = names.iterator(); iterator.hasNext()
-                && !found;) {
-            String filename = iterator.next();
-            LOGGER.debug("Trying file " + filename);
-            File file = new File(filename);
-            if (file.exists()) {
-                FileInputStream inStream = null;
-                try {
-                    inStream = new FileInputStream(file);
-                } catch (java.io.FileNotFoundException e) {
-                    throw new ProblematicConfiguationFileException(filename);
-                }
-                if (inStream != null) {
-                    try {
-                        props.load(inStream);
-                    } catch (IOException e) {
-                        throw new ProblematicConfiguationFileException(filename);
-                    }
-                    this.properties = props;
-                    found = true;
-                }
+        InputStream inputStream = null;
+        final ClassLoader loader = ClassLoader.getSystemClassLoader();
+        if (loader != null) {
+            URL url = loader.getResource(this.configFilename);
+            if (url == null) {
+                url = loader.getResource("/" + this.configFilename);
             }
-        }
-        if (!found) {
-            readFileFromClasspath(this.confFilename);
-            if (this.properties == null) {
-                LOGGER.error("ERROR not configured");
-                throw new ProblematicConfiguationFileException("file");
+            if (url != null) {
+                try {
+                    // This file has the properties.
+                    inputStream = url.openStream();
+                    // Put the values in the properties.
+                    this.properties = new Properties();
+                    this.properties.load(inputStream);
+                } catch (final IOException e) {
+                    LOGGER.error(e.getMessage());
+                    throw new ProblematicConfiguationFileException(
+                            url.getFile());
+                } finally {
+                    try {
+                        if (inputStream != null) {
+                            inputStream.close();
+                        }
+                    } catch (final IOException e) {
+                        LOGGER.error(e.getMessage());
+                        throw new ProblematicConfiguationFileException(
+                                url.getFile());
+                    }
+                }
+            } else {
+                LOGGER.error("URL not found");
             }
         }
 
@@ -194,37 +190,36 @@ public class Configurator {
     }
 
     /**
-     * Getter
-     * 
-     * @return
+     * Retrieves the configuration file.
+     *
+     * @return The path to the configuration file name.
      */
-    String getConfFilename() {
-        LOGGER.trace(">< getConfFilename");
+    String getConfigFilename() {
+        LOGGER.trace(">< getConfigFilename");
 
-        return this.confFilename;
+        return this.configFilename;
     }
 
     /**
      * Find the value for a defined parameter. If not present, throws a
-     * ConfigNotFoundException
-     * 
+     * KeyNotFoundException.
+     *
      * @param sec
-     *            the section
+     *            Section of the property.
      * @param key
-     *            the key
-     * @return the value
+     *            Name of the property.
+     * @return The value.
+     * @throws KeyNotFoundException
+     *             If the variable was not found.
      * @throws ProblematicConfiguationFileException
      *             If there is a problem reading the file.
      */
-    public String getValue(String sec, String key)
-            throws ConfigNotFoundException,
-            ProblematicConfiguationFileException {
+    public String getValue(final String sec, final String key)
+            throws KeyNotFoundException, ProblematicConfiguationFileException {
         LOGGER.trace("> getValue");
 
-        assert sec != null;
-        assert !sec.equals("");
-        assert key != null;
-        assert !key.equals("");
+        assert sec != null && !sec.equals("");
+        assert key != null && !key.equals("");
 
         if (this.properties == null) {
             this.findConfigPath();
@@ -234,106 +229,61 @@ public class Configurator {
 
         if (value == null) {
             LOGGER.debug("Nothing found for [" + sec + "]:" + key);
-            throw new ConfigNotFoundException(sec, key);
+            throw new KeyNotFoundException(sec, key);
         }
+
+        assert value != null;
 
         LOGGER.trace("< getValue");
 
         return value;
     }
 
-    private void readFileFromClasspath(final String/* ! */filename)
-            throws ProblematicConfiguationFileException {
-        LOGGER.trace("> readFileFromClasspath");
-
-        assert filename != null;
-
-        InputStream inputStream = null;
-        final ClassLoader loader = ClassLoader.getSystemClassLoader();
-        if (loader != null) {
-            URL url = loader.getResource(filename);
-            if (url == null) {
-                url = loader.getResource("/" + filename);
-            }
-            if (url != null) {
-                try {
-                    // This file has the properties.
-                    inputStream = url.openStream();
-                    // The file is XML and put the values in the properties.
-                    this.properties = new Properties();
-                    this.properties.load(inputStream);
-                } catch (final InvalidPropertiesFormatException e) {
-                    LOGGER.error(e.getMessage());
-                    throw new ProblematicConfiguationFileException(url
-                            .getFile());
-                } catch (final IOException e) {
-                    LOGGER.error(e.getMessage());
-                    throw new ProblematicConfiguationFileException(url
-                            .getFile());
-                } finally {
-                    try {
-                        if (inputStream != null) {
-                            inputStream.close();
-                        }
-                    } catch (final IOException e) {
-                        LOGGER.error(e.getMessage());
-                        throw new ProblematicConfiguationFileException(url
-                                .getFile());
-                    }
-                }
-            } else {
-                LOGGER.error("URL not found");
-            }
-        }
-
-        LOGGER.trace("< readFileFromClasspath");
-    }
-
     /**
-     * Setter
-     * 
+     * Setter for the configuration file name.
+     *
      * @param filename
+     *            Configuration file name.
      * @throws ProblematicConfiguationFileException
      *             If there is a problem reading the file.
      */
-    public void setConfFilename(String filename)
+    public void setFilename(final String filename)
             throws ProblematicConfiguationFileException {
-        LOGGER.trace("> setConfFilename");
+        LOGGER.trace("> setFilename");
 
-        assert filename != null;
-        assert !filename.equals("");
+        assert filename != null && !filename.equals("");
 
-        this.confFilename = filename;
+        this.configFilename = filename;
 
         this.findConfigPath();
 
-        LOGGER.trace("< setConfFilename");
+        LOGGER.trace("< setFilename");
     }
 
     /**
-     * Sets value, For testing purposes.
-     * 
-     * @param sec
+     * Sets value. For testing purposes.
+     *
+     * @param section
+     *            Section in configuration file.
      * @param key
+     *            Identifier of the variable.
      * @param value
+     *            Value of the variable.
      * @throws ProblematicConfiguationFileException
      *             If there is a problem reading the file.
      */
-    public void setValue(String sec, String key, String value)
-            throws ProblematicConfiguationFileException {
+    public void setValue(final String section, final String key,
+            final String value) throws ProblematicConfiguationFileException {
         LOGGER.trace("> setValue");
 
-        assert sec != null;
-        assert !sec.equals("");
-        assert key != null;
-        assert !key.equals("");
-        assert value != null;
-        assert !value.equals("");
+        assert section != null && !section.equals("");
+        assert key != null && !key.equals("");
+        assert value != null && !value.equals("");
 
         if (this.properties == null) {
             findConfigPath();
         }
-        this.properties.setProperty(sec + "." + key, value);
+        this.properties.setProperty(section + "." + key, value);
 
         LOGGER.trace("< setValue");
     }
