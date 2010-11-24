a2f9a52f410ed0f8f597477539bc6b618b93140e
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-11-24 19:51:57 +0100

    Main method of the application reviewed

--- src/main/java/fr/in2p3/cc/storage/treqs/main/Main.java
+++ src/main/java/fr/in2p3/cc/storage/treqs/main/Main.java
@@ -1,20 +1,74 @@
+/*
+ * Copyright      Jonathan Schaeffer 2009-2010,
+ *                  CC-IN2P3, CNRS <jonathan.schaeffer@cc.in2p3.fr>
+ * Contributors   Andres Gomez,
+ *                  CC-IN2P3, CNRS <andres.gomez@cc.in2p3.fr>
+ *
+ * This software is a computer program whose purpose is to schedule, sort
+ * and submit file requests to the hierarchical storage system HPSS.
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights, and the successive licensors have only limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ *
+ */
 package fr.in2p3.cc.storage.treqs.main;
 
 import fr.in2p3.cc.storage.treqs.control.starter.Starter;
 
-public class Main {
+/**
+ * Main method of the application.
+ *
+ * @author Andrés Gómez
+ * @since 1.5
+ */
+public final class Main {
 
     /**
+     * Main method of the application, if there is an exception it will print
+     * the info.
+     *
      * @param args
+     *            Arguments for the loading.
      * @throws InterruptedException
+     *             When there is a problem with the threads.
      */
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(final String[] args) throws InterruptedException {
         System.out.println("jTReqS-Server started");
         try {
             new Starter().process(args);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Default constructor hidden.
+     */
+    private Main() {
+        // Nothing
     }
 
 }
