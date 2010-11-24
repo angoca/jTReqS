197a2e74bfa0b529d25c0d7906b07ef18e020d8a
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-11-24 15:09:52 +0100

    final classes

--- src/main/java/fr/in2p3/cc/storage/treqs/persistence/PersistenceFactoryException.java
+++ src/main/java/fr/in2p3/cc/storage/treqs/persistence/PersistenceFactoryException.java
@@ -42,7 +42,7 @@ package fr.in2p3.cc.storage.treqs.persistence;
  * @author Andrés Gómez
  * @since 1.5
  */
-public class PersistenceFactoryException extends PersistanceException {
+public final class PersistenceFactoryException extends PersistanceException {
 
     /**
      * Generated ID.
