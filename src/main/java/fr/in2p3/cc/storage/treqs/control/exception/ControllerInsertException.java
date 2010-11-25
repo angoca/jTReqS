739a4658a20d3aa035d5bae88aa46b0f2df97e9e
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-11-25 14:47:20 +0100

    Abstract class renamed

--- src/main/java/fr/in2p3/cc/storage/treqs/control/exception/ControllerInsertException.java
+++ src/main/java/fr/in2p3/cc/storage/treqs/control/exception/ControllerInsertException.java
@@ -45,7 +45,7 @@ import org.slf4j.LoggerFactory;
  * @author Andrés Gómez
  * @since 1.5
  */
-public final class ControllerInsertException extends ControllerException {
+public final class ControllerInsertException extends AbstractControllerException {
     /**
      * Logger.
      */
