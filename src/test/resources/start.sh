4187388ba9f82413ca48504bc4a68c10e3ee27a4
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-12-21 19:15:00 +0100

    Batch started

--- src/test/resources/start.sh
+++ src/test/resources/start.sh
@@ -14,14 +14,14 @@ SLF4J=${REPOSITORY}/slf4j/slf4j-1.6.1/slf4j-api-1.6.1.jar
 
 CLASSPATH=.:${COMMONS_CLI}:${COMMONS_CONFIGURATION}:${COMMONS_LANG}:${COMMONS_LOGGING}:${COMMONS_COLLECTIONS}:${LOGBACK_CLASSIC}:${LOGBACK_CORE}:${MYSQL}:${SLF4J}
 
-LOGBACK_CONF=-Dlogback.configurationFile=logback-test.xml
-CONF_FILE=--config treqs.conf.test.properties
+LOGBACK_CONF="-Dlogback.configurationFile=logback-test.xml"
+CONF_FILE="-c treqs.conf.test.properties"
 
-MAIN=fr.in2p3.cc.storage.treqs.main.Main
+MAIN="fr.in2p3.cc.storage.treqs.main.Main"
 
-ASSERTIONS=-ea
+ASSERTIONS="-ea"
 
-CMD="java -cp ${CLASSPATH} ${ASSERTIONS} ${LOGBACK_CONF} ${MAIN} ${CONF_FILE}"
+CMD="java -cp ${CLASSPATH} ${ASSERTIONS} ${LOGBACK_CONF} ${MAIN} ${CONF_FILE} $1"
 
 echo sudo /etc/init.d/mysqld start
 echo ${CMD}
