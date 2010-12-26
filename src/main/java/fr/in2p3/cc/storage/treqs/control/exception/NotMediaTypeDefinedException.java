a1bb675987f8026cbc35030020cdbc7157d07429
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2010-12-26 23:32:16 +0100

    Format

--- src/main/java/fr/in2p3/cc/storage/treqs/control/exception/NotMediaTypeDefinedException.java
+++ src/main/java/fr/in2p3/cc/storage/treqs/control/exception/NotMediaTypeDefinedException.java
@@ -8,28 +8,31 @@ import fr.in2p3.cc.storage.treqs.TReqSException;
 /**
  * The media type has not been defined. Probably, this is a type that has not
  * been defined in the database.
- * 
+ *
  * @author Andrés Gómez
  * @since 1.5
  */
 public final class NotMediaTypeDefinedException extends TReqSException {
 
-	/**
-	 * Logger.
-	 */
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(NotMediaTypeDefinedException.class);
-	/**
-	 * Generated ID
-	 */
-	private static final long serialVersionUID = -2897346370815410476L;
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(NotMediaTypeDefinedException.class);
+    /**
+     * Generated ID
+     */
+    private static final long serialVersionUID = -2897346370815410476L;
 
-	/**
-	 * Creates the exception.
-	 */
-	public NotMediaTypeDefinedException() {
-		super();
+    /**
+     * Creates the exception.
+     *
+     * @param storageName
+     *            Name of the storage name.
+     */
+    public NotMediaTypeDefinedException(final String storageName) {
+        super(storageName);
 
-		LOGGER.trace(">< Instance creation");
-	}
+        LOGGER.trace(">< Instance creation");
+    }
 }
