678f6132704f08a86c2c0bb9a576229fa41e4ebe
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2011-01-07 15:47:50 +0100

    Test reviewed

---
+++ src/test/java/fr/in2p3/cc/storage/treqs/persistence/mysql/MySQLInitTest.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright      Jonathan Schaeffer 2009-2010,
+ *                  CC-IN2P3, CNRS <jonathan.schaeffer@cc.in2p3.fr>
+ * Contributors   Andres Gomez,
+ *                  CC-IN2P3, CNRS <andres.gomez@cc.in2p3.fr>
+ *
+ * This software is a computer program whose purpose is to schedule, sort
+ * and submit file requests to the hierarchical storage system HPSS.
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights, and the successive licensors have only limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ *
+ */
+package fr.in2p3.cc.storage.treqs.persistence.mysql;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import fr.in2p3.cc.storage.treqs.Constants;
+import fr.in2p3.cc.storage.treqs.MainTests;
+import fr.in2p3.cc.storage.treqs.TReqSException;
+import fr.in2p3.cc.storage.treqs.persistence.mysql.MySQLInit;
+import fr.in2p3.cc.storage.treqs.persistence.mysql.MySQLBroker;
+import fr.in2p3.cc.storage.treqs.persistence.mysql.exception.AbstractMySQLException;
+import fr.in2p3.cc.storage.treqs.tools.Configurator;
+
+/**
+ * Test to init the database.
+ *
+ * @author Andrés Gómez
+ */
+public final class MySQLInitTest {
+
+    /**
+     * Drops a table.
+     *
+     * @param table
+     *            Table to drop.
+     * @throws TReqSException
+     *             Never.
+     */
+    private static void dropTable(final String table) throws TReqSException {
+        try {
+            MySQLBroker.getInstance().executeModification(
+                    "DROP TABLE IF EXISTS " + table);
+        } catch (AbstractMySQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Init the test.
+     *
+     * @throws TReqSException
+     *             If there is a problem deleting the tables.
+     */
+    @BeforeClass
+    public static void oneTimeSetUp() throws TReqSException {
+        System.setProperty(Constants.CONFIGURATION_FILE,
+                MainTests.PROPERTIES_FILE);
+        MySQLBroker.getInstance().connect();
+        dropTable(MySQLStatements.REQUESTS);
+        dropTable(MySQLStatements.QUEUES);
+        dropTable(MySQLStatements.ALLOCATIONS);
+        dropTable(MySQLStatements.MEDIATYPES);
+        dropTable(MySQLStatements.HEART_BEAT);
+        MySQLBroker.getInstance().disconnect();
+    }
+
+    /**
+     * Finalizes the test.
+     */
+    @AfterClass
+    public static void oneTimeTearDown() {
+        MySQLBroker.destroyInstance();
+        Configurator.destroyInstance();
+    }
+
+    /**
+     * Create the first time.
+     *
+     * @throws TReqSException
+     *             Never.
+     */
+    @Test
+    public void test01create() throws TReqSException {
+        new MySQLInit().initializeDatabase();
+    }
+
+    /**
+     * Not create.
+     *
+     * @throws TReqSException
+     *             Never.
+     */
+    @Test
+    public void test02create() throws TReqSException {
+        new MySQLInit().initializeDatabase();
+    }
+
+}
