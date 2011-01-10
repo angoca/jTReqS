d1c14a9f2c12e54e01f39b9e3965a0804f6a9651
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2011-01-10 19:15:47 +0100

    imports

--- src/test/java/fr/in2p3/cc/storage/treqs/model/ReadingTest.java
+++ src/test/java/fr/in2p3/cc/storage/treqs/model/ReadingTest.java
@@ -56,8 +56,6 @@ import fr.in2p3.cc.storage.treqs.hsm.exception.HSMStageException;
 import fr.in2p3.cc.storage.treqs.hsm.mock.HSMMockBridge;
 import fr.in2p3.cc.storage.treqs.model.exception.InvalidStatusTransitionException;
 import fr.in2p3.cc.storage.treqs.persistence.AbstractDAOFactory;
-import fr.in2p3.cc.storage.treqs.persistence.mysql.MySQLBroker;
-import fr.in2p3.cc.storage.treqs.persistence.mysql.MySQLStatements;
 import fr.in2p3.cc.storage.treqs.tools.Configurator;
 
 /**
@@ -93,9 +91,6 @@ public final class ReadingTest {
 
     /**
      * Destroys all after tests.
-     *
-     * @throws TReqSException
-     *             Problem acceding the database.
      */
     @AfterClass
     public static void oneTimeTearDown() {
