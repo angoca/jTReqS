d2cff83845404a2942b0aa171aa85b379fd898fa
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2011-01-12 12:44:59 +0100

    Reviewed with TODOs

--- src/test/java/fr/in2p3/cc/storage/treqs/hsm/hpssJNI/HPSSJNIBridgeTester.java
+++ src/test/java/fr/in2p3/cc/storage/treqs/hsm/hpssJNI/HPSSJNIBridgeTester.java
@@ -1,12 +1,57 @@
+/*
+ * Copyright      Jonathan Schaeffer 2009-2010,
+ *                  CC-IN2P3, CNRS <jonathan.schaeffer@cc.in2p3.fr>
+ * Contributors   Andres Gomez,
+ *                  CC-IN2P3, CNRS <andres.gomez@cc.in2p3.fr>
+ *
+ * This software is a computer program whose purpose is to schedule, sort
+ * and submit file requests to the hierarchical storage system HPSS.
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights, and the successive licensors have only limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ *
+ */
 package fr.in2p3.cc.storage.treqs.hsm.hpssJNI;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fr.in2p3.cc.storage.treqs.model.exception.TReqSException;
+import fr.in2p3.cc.storage.treqs.Constants;
+import fr.in2p3.cc.storage.treqs.TReqSException;
+import fr.in2p3.cc.storage.treqs.model.File;
 import fr.in2p3.cc.storage.treqs.tools.Configurator;
 
-public class HPSSJNIBridgeTester {
+/**
+ * Test the JNI bridge.
+ * <p>
+ * TODO convert to tests.
+ *
+ * @author Andres Gomez
+ */
+public final class HPSSJNIBridgeTester {
     /**
      * Logger.
      */
@@ -25,23 +70,33 @@ public class HPSSJNIBridgeTester {
      * @throws TReqSException
      *             If there is any error.
      */
-    public static void main(String[] args) throws TReqSException {
+    public static void main(final String[] args) throws TReqSException {
         LOGGER.error("> Starting HPSSBridge");
+
+        String[] arguments = args;
         String fileName = "";
-        if (args.length == 0) {
-            args = new String[] { "/var/hpss/etc/keytab.treqs",
+        if (arguments.length == 0) {
+            arguments = new String[] { "/var/hpss/etc/keytab.treqs",
                     "/hpss/in2p3.fr/group/ccin2p3/treqs/dummy" };
-        } else if (args.length > 1 && !args[1].equals("")) {
-            fileName = args[1];
+        } else if (arguments.length > 1 && !arguments[1].equals("")) {
+            fileName = arguments[1];
         } else {
             fileName = "/hpss";
         }
-        LOGGER.error("Keytab: {}, File {}", args);
-        Configurator.getInstance().setValue("MAIN", "KEYTAB_FILE", args[0]);
+        LOGGER.error("Keytab: {}, File {}", arguments);
+        Configurator.getInstance().setValue(Constants.SECTION_KEYTAB,
+                Constants.KEYTAB_FILE, arguments[0]);
         LOGGER.error("Getting properties");
         HPSSJNIBridge.getInstance().getFileProperties(fileName);
         LOGGER.error("Staging file");
-        HPSSJNIBridge.getInstance().stage(fileName, 1);
+        HPSSJNIBridge.getInstance().stage(new File(fileName, 1));
         LOGGER.error(";)");
+    }
+
+    /**
+     * Hidden.
+     */
+    private HPSSJNIBridgeTester() {
+        // Nothing
     }
 }
