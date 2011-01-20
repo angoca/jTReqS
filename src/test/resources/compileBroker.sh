d2d756f3fbac108c30bb5aad21e4b92728f7afdc
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2011-01-20 17:37:00 +0100

    dot slash

--- src/test/resources/compileBroker.sh
+++ src/test/resources/compileBroker.sh
@@ -1,3 +1,3 @@
 # HPSS Broker
 echo Compiling HPSS Broker
-gcc -I /opt/hpss/include/ -DLINUX -fPIC -o HPSSBroker.o -c ../src/main/c/HPSSBroker.c
+gcc -I /opt/hpss/include/ -DLINUX -fPIC -o ./HPSSBroker.o -c ../src/main/c/HPSSBroker.c
