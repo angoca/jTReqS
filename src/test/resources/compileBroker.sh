86ec934e791e828ce7b51ce9ef5048ebfa7610af
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2011-01-21 18:18:48 +0100

    Execution instruction

--- src/test/resources/compileBroker.sh
+++ src/test/resources/compileBroker.sh
@@ -1,4 +1,9 @@
 # HPSS Broker
+#
+# This script should be executed in the 'bin' directory of the project.
+#
+# @author Andres Gomez
+
 echo Compiling HPSS Broker
 rm -f ./HPSSBroker.o
 gcc -I /opt/hpss/include/ -DLINUX -fPIC -o ./HPSSBroker.o -c ../src/main/c/HPSSBroker.c
