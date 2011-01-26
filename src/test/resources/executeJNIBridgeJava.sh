eede1d41b19213b4b075cf080117ef1cfd3e050c
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2011-01-26 14:23:58 +0100

    less output from treqs and hpss, and assertions enabled

--- src/test/resources/executeJNIBridgeJava.sh
+++ src/test/resources/executeJNIBridgeJava.sh
@@ -12,10 +12,10 @@ javac -cp . -d . ../src/test/java/fr/in2p3/cc/storage/treqs/hsm/hpssJNI/HPSSJNIB
 
 echo Executing
 # This is for HPSS logging (it works from 0 - 7, the three bits)
-export HPSS_API_DEBUG=7
+export HPSS_API_DEBUG=0
 # This is for the internal logger (WARN, INFO, DEBUG, TRACE)
-export TREQS_LOG=TRACE
+export TREQS_LOG=WARN
 export LD_LIBRARY_PATH=`pwd`:/opt/hpss/lib/
 
-java -Djava.library.path=./ -cp ./ fr.in2p3.cc.storage.treqs.hsm.hpssJNI.HPSSJNIBridgeTester
+java -Djava.library.path=./ -ea -cp ./ fr.in2p3.cc.storage.treqs.hsm.hpssJNI.HPSSJNIBridgeTester
 
