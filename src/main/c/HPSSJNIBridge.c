b1cad95e460899eca71b6f9b195592d03d81e777
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2011-01-26 11:27:25 +0100

    JNI almost working

--- src/main/c/HPSSJNIBridge.c
+++ src/main/c/HPSSJNIBridge.c
@@ -1,4 +1,4 @@
- /*
+/*
  * Copyright      Jonathan Schaeffer 2009-2010,
  *                  CC-IN2P3, CNRS <jonathan.schaeffer@cc.in2p3.fr>
  * Contributors   Andres Gomez,
@@ -35,172 +35,229 @@
  *
  */
 #include <stdlib.h>
-#include "fr_in2p3_cc_storage_treqs_hsm_hpssJNI_HPSSJNIBridge.h"
+#include "fr_in2p3_cc_storage_treqs_hsm_hpssJNI_NativeBridge.h"
 #include "HPSSBroker.h"
+#include "ProcessException.h"
 
 #define cont (rc == HPSS_E_NOERROR)
+#define trace (LOGGER != NULL && strcmp(LOGGER, "TRACE") == 0)
+#define debug (LOGGER != NULL && (strcmp(LOGGER, "TRACE") || strcmp(LOGGER, "DEBUG")) == 0)
+#define info (LOGGER != NULL && (strcmp(LOGGER, "TRACE") || strcmp(LOGGER, "DEBUG") || strcmp(LOGGER, "INFO")) == 0)
+#define warn (LOGGER != NULL && (strcmp(LOGGER, "TRACE") || strcmp(LOGGER, "DEBUG") || strcmp(LOGGER, "INFO") || strcmp(LOGGER, "WARN")) == 0)
 
 char* LOGGER;
 
-JNIEXPORT void JNICALL Java_fr_in2p3_cc_storage_treqs_hsm_hpssJNI_HPSSJNIBridge_hpssInit(
-    JNIEnv* env, jclass js, jstring jAuthType, jstring jKeytab,
-    jstring jUser) {
-  const char * authType;
-  const char * keytab;
-  const char * user;
-  jclass hsmInitException;
-  jint rc;
+JNIEXPORT jobject JNICALL Java_fr_in2p3_cc_storage_treqs_hsm_hpssJNI_NativeBridge_getFileProperties(
+		JNIEnv* env, jclass js, jstring jFileName) {
 
-  LOGGER = getenv("TREQS_TRACE");
-  if (strcmp(LOGGER, "TRACE") == 0) {
-    printf("> hpssInit\n");
-  }
+	char filename[HPSS_MAX_PATH_NAME + HPSS_MAX_FILE_NAME + 1];
 
-  // Converts Java Strings in char*;
-  authType = (*env)->GetStringUTFChars(env, jAuthType, JNI_FALSE);
-  keytab = (*env)->GetStringUTFChars(env, jKeytab, JNI_FALSE);
-  user = (*env)->GetStringUTFChars(env, jUser, JNI_FALSE);
+	int position;
+	int storageLevel;
+	char tape[HPSS_PV_NAME_SIZE + 1];
+	unsigned long long size;
 
-  // Calls the Broker.
-  rc = init(authType, keytab, user);
+	int rc = -1;
 
-  // Release JNI components.
-  (*env)->ReleaseStringUTFChars(env, jAuthType, authType);
-  (*env)->ReleaseStringUTFChars(env, jKeytab, keytab);
-  (*env)->ReleaseStringUTFChars(env, jUser, user);
+	char message[1024];
 
-  // Throws an exception if there is a problem.
-  if (rc != HPSS_E_NOERROR) {
-    hsmInitException = (*env)->FindClass(env,
-        "fr.in2p3.cc.storage.treqs.hsm.exception.AbstractHSMInitException");
-    // FIXME there is a problem here while throwing the exception.
-    //		(*env)->ThrowNew(env, hsmInitException, "" + rc);
-  }
+	// JNI
+	jstring jTapeName;
+	jclass helperClass;
+	jmethodID cid;
+	jobject result = NULL;
 
-  if (strcmp(LOGGER, "TRACE") == 0) {
-    printf("< hpssInit\n");
-  }
+	if (trace) {
+		printf("> JNI getFileProperties\n");
+	}
+
+	// Converts JavaString in char*.
+	filename = (*env)->GetStringUTFChars(env, jFileName, JNI_FALSE);
+
+	// Calls the broker.
+	rc = getFileProperties(filename, &position, &storageLevel, tape, &size);
+
+	// Release JNI component.
+	(*env)->ReleaseStringUTFChars(env, jFileName, filename);
+	// FIXME how to use file and tape here.
+	printf("file %s\n", filename);
+	printf("POSITION %d\n", position);
+	printf("size %lld\n", size);
+	printf("tape %s\n", tape);
+
+	// Throws an exception if there is a problem.
+	if (rc != HPSS_E_NOERROR) {
+		sprintf(message, "getProperties %d", rc);
+
+		throwJNIException(env, message);
+	}
+
+	if (cont) {
+		if (debug) {
+			printf("Preparing results\n");
+		}
+		// Prepares the value when the file is already in disk.
+		if (storageLevel == 0) {
+			// This is the value of the constant Constants.FILE_ON_DISK
+			strcpy(tape, "DISK");
+			position = -1;
+		}
+
+		// Returns the elements to java and release JNI components.
+
+		// Prepares the StorageLevel - StorageName
+		jTapeName = (*env)->NewStringUTF(env, tape);
+		if (jTapeName == NULL) {
+			throwJNIException(env, "Problem creating the string - tapename");
+			rc = -1;
+		}
+	}
+	if (cont) {
+		// Attempt to find the HSMHelper class.
+		helperClass = (*env)->FindClass(env,
+				"fr/in2p3/cc/storage/treqs/hsm/HSMHelperFileProperties");
+		// If this class does not exist then return null.
+		if (helperClass == NULL) {
+			throwJNIException(env, "Class not found");
+			rc = -1;
+		}
+	}
+	if (cont) {
+		// Get the method ID for the Helper(String, int, long) constructor */
+		cid = (*env)->GetMethodID(env, helperClass, "<init>",
+				"(Ljava/lang/String;IJ)V");
+		if (cid == NULL) {
+			throwJNIException(env, "Constructor not found");
+			rc = -1;
+		}
+	}
+	if (cont) {
+		// Creates the object.
+		result
+				= (*env)->NewObject(env, helperClass, cid, jTapeName, position, size);
+
+		/* Free local references */
+		(*env)->DeleteLocalRef(env, helperClass);
+	}
+
+	if (trace) {
+		printf("< JNI getFileProperties\n");
+	}
+
+	return result;
 }
 
-JNIEXPORT jint JNICALL Java_fr_in2p3_cc_storage_treqs_hsm_hpssJNI_HPSSBridge_getFileProperties(
-    JNIEnv* env, jclass js, jstring jFileName, jobject helper) {
+JNIEXPORT void JNICALL Java_fr_in2p3_cc_storage_treqs_hsm_hpssJNI_NativeBridge_init(
+		JNIEnv* env, jclass js, jstring jAuthType, jstring jKeytab, jstring jUser) {
+	const char * authType;
+	const char * keytab;
+	const char * user;
 
-  const char * filename;
+	int rc = -1;
 
-  int position;
-  int storageLevel;
-  char tape[12];
-  unsigned long length;
-  unsigned long long int size = 0;
+	char message[1024];
 
-  // JNI
-  jclass jclazz;
-  jmethodID jmethodSetPosition;
-  jmethodID jmethodSetStorageName;
-  jmethodID jmethodSetSize;
-  jstring jtape;
-  jclass hsmInitException;
-  jint rc = 0;
+	LOGGER = getenv("TREQS_LOG");
+	if (trace) {
+		printf("> JNI init\n");
+	}
 
-  if (strcmp(LOGGER, "TRACE") == 0) {
-    printf("> getFileProperties\n");
-  }
+	// Converts Java Strings in char*;
+	authType = (*env)->GetStringUTFChars(env, jAuthType, JNI_FALSE);
+	keytab = (*env)->GetStringUTFChars(env, jKeytab, JNI_FALSE);
+	user = (*env)->GetStringUTFChars(env, jUser, JNI_FALSE);
 
-  // Converts JavaString in char*.
-  filename = (*env)->GetStringUTFChars(env, jFileName, JNI_FALSE);
+	// Calls the Broker.
+	rc = init(authType, keytab, user);
 
-  // Calls the broker.
-  rc = getFileProperties(filename, &position, &storageLevel, tape, &length);
+	// Release JNI components.
+	(*env)->ReleaseStringUTFChars(env, jAuthType, authType);
+	(*env)->ReleaseStringUTFChars(env, jKeytab, keytab);
+	(*env)->ReleaseStringUTFChars(env, jUser, user);
 
-  // Release JNI component.
-  (*env)->ReleaseStringUTFChars(env, jFileName, filename);
+	// Throws an exception if there is a problem.
+	if (rc != HPSS_E_NOERROR) {
+		sprintf(message, "init %d", rc);
+		throwJNIException(env, message);
+	}
 
-  // Throws an exception if there is a problem.
-  if (rc != HPSS_E_NOERROR) {
-    hsmInitException = (*env)->FindClass(env,
-        "fr.in2p3.cc.storage.treqs.hsm.exception.AbstractHSMInitException");
-    (*env)->ThrowNew(env, hsmInitException, "" + rc);
-  }
-
-  if (strcmp(LOGGER, "TRACE") == 0) {
-    printf("Converting results\n");
-  }
-  // Returns the elements to java and release JNI components.
-  // Position.
-  if (cont) {
-    // This does not seem to work on 64b
-    // CONVERT_U64_TO_LONGLONG(length, *s);
-    // TODO (jschaeff) this conversion is ugly
-    size = atoll(u64tostr(length));
-    if (storageLevel == 0) {
-      strcpy(tape, "DISK");
-      position = -1;
-    }
-
-    jclazz = (*env)->GetObjectClass(env, helper);
-    jmethodSetPosition = (*env)->GetMethodID(env, jclazz, "setPosition",
-        "(I)V");
-    if (jmethodSetPosition == 0) {
-      printf("Can't find method setPosition");
-      return -30001;
-    }
-    (*env)->ExceptionClear(env);
-    (*env)->CallVoidMethod(env, helper, jmethodSetPosition, position);
-    if ((*env)->ExceptionOccurred(env)) {
-      printf("Error occurred copying array back (setPosition)");
-      (*env)->ExceptionDescribe(env);
-      (*env)->ExceptionClear(env);
-    }
-  }
-  // StorageLevel - StorageName
-  if (cont) {
-    jmethodSetStorageName = (*env)->GetMethodID(env, jclazz,
-        "setStorageName", "(Ljava/lang/String;)V");
-    if (jmethodSetStorageName == 0) {
-      printf("Can't find method setStorageName");
-      return -30002;
-    }
-    (*env)->ExceptionClear(env);
-    jtape = (*env)->NewStringUTF(env, tape);
-    (*env)->CallVoidMethod(env, helper, jmethodSetStorageName, jtape);
-    if ((*env)->ExceptionOccurred(env)) {
-      printf("Error occurred copying array back (setStorageName)");
-      (*env)->ExceptionDescribe(env);
-      (*env)->ExceptionClear(env);
-    }
-  }
-  // Size
-  if (cont) {
-    jmethodSetSize = (*env)->GetMethodID(env, jclazz, "setSize", "(J)V");
-    if (jmethodSetSize == 0) {
-      printf("Can't find method setSize");
-      return -30003;
-    }
-    (*env)->ExceptionClear(env);
-    (*env)->CallVoidMethod(env, helper, jmethodSetSize, size);
-    if ((*env)->ExceptionOccurred(env)) {
-      printf("Error occurred copying array back (setSize)");
-      (*env)->ExceptionDescribe(env);
-      (*env)->ExceptionClear(env);
-    }
-  }
-
-  if (strcmp(LOGGER, "TRACE") == 0) {
-    printf("< getFileProperties\n");
-  }
-
-  return rc;
+	if (trace) {
+		printf("< JNI init\n");
+	}
 }
 
-JNIEXPORT jint JNICALL Java_fr_in2p3_cc_storage_treqs_hsm_hpssJNI_HPSSBridge_stage(
-    JNIEnv* env, jclass js, jstring jFileName, jlong size) {
+JNIEXPORT void JNICALL Java_fr_in2p3_cc_storage_treqs_hsm_hpssJNI_NativeBridge_stage(
+		JNIEnv* env, jclass js, jstring jFileName, jlong size) {
+	char * filename;
 
-    // FIXME
-  // Throws an exception if there is a problem.
-  if (rc != HPSS_E_NOERROR) {
-    hsmInitException = (*env)->FindClass(env,
-        "fr.in2p3.cc.storage.treqs.hsm.exception.AbstractHSMInitException");
-    (*env)->ThrowNew(env, HSMResourceException, rc);
-  }
+	int rc = -1;
+	char message[1024];
 
+	if (trace) {
+		printf("> stage\n");
+	}
+
+	// Converts Java Strings in char*;
+	filename = (*env)->GetStringUTFChars(env, jFileName, JNI_FALSE);
+
+	// Calls the Broker.
+	// FIXME size is jint and it is not a long long int.
+	rc = stage(filename, &size);
+
+	// Release JNI components.
+	(*env)->ReleaseStringUTFChars(env, jFileName, filename);
+
+	// Throws an exception if there is a problem.
+	if (rc != HPSS_E_NOERROR) {
+		sprintf(message, "stage %d", rc);
+		throwJNIException(env, message);
+	}
+
+	if (trace) {
+		printf("< stage\n");
+	}
+}
+
+jint throwNoClassDefError(JNIEnv *env, const char *message) {
+	jclass exClass;
+	const char *className = "java/lang/NoClassDefFoundError";
+
+	if (trace) {
+		printf("> Creation generic exception\n");
+	}
+
+	exClass = (*env)->FindClass(env, className);
+	if (exClass == NULL) {
+		exit(-1);
+	}
+
+	if (trace) {
+		printf("< Creation generic exception\n");
+	}
+
+	return (*env)->ThrowNew(env, exClass, message);
+}
+
+jint throwJNIException(JNIEnv *env, char *message) {
+	jclass exClass;
+	const char *className =
+			"fr/in2p3/cc/storage/treqs/hsm/hpssJNI/exception/JNIException";
+
+	if (trace) {
+		printf("> Creation JNI exception\n");
+	}
+
+	exClass = (*env)->FindClass(env, className);
+	if (exClass == NULL) {
+		if (warn) {
+			printf("Unable to found exception\n");
+		}
+		return throwNoClassDefError(env, className);
+	}
+
+	if (trace) {
+		printf("< Creation JNI exception\n");
+	}
+
+	return (*env)->ThrowNew(env, exClass, message);
 }
