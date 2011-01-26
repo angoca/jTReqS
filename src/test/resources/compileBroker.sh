7ec9b28e6a971064ab85eb5b731561a91021fc4c
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2011-01-26 10:37:06 +0100

    Wall param for compilation

--- src/test/resources/compileBroker.sh
+++ src/test/resources/compileBroker.sh
@@ -6,4 +6,4 @@
 
 echo Compiling HPSS Broker
 rm -f ./HPSSBroker.o
-gcc -I /opt/hpss/include/ -DLINUX -fPIC -o ./HPSSBroker.o -c ../src/main/c/HPSSBroker.c
+gcc -I /opt/hpss/include/ -DLINUX -fPIC -Wall -o ./HPSSBroker.o -c ../src/main/c/HPSSBroker.c
