0fa330a5919eb96457dfd2b032beb73f26a068b9
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2011-02-07 19:16:49 +0100

    New directory

--- java/src/test/resources/compileJNIBridgeJava.sh
+++ java/src/test/resources/compileJNIBridgeJava.sh
@@ -8,15 +8,15 @@
 sh ./compileBroker.sh
 
 echo Generating JNI Bridge - Java
-javac -cp . -d ./ ../src/main/java/fr/in2p3/cc/storage/treqs/hsm/HSMHelperFileProperties.java -encoding UTF8
-javac -cp . -d ./ ../src/main/java/fr/in2p3/cc/storage/treqs/hsm/hpssJNI/JNIException.java -encoding UTF8
-javac -cp . -d ./ ../src/main/java/fr/in2p3/cc/storage/treqs/hsm/hpssJNI/NativeBridge.java -encoding UTF8
+javac -cp . -d ./ ../java/src/main/java/fr/in2p3/cc/storage/treqs/hsm/HSMHelperFileProperties.java -encoding UTF8
+javac -cp . -d ./ ../java/src/main/java/fr/in2p3/cc/storage/treqs/hsm/hpssJNI/JNIException.java -encoding UTF8
+javac -cp . -d ./ ../java/src/main/java/fr/in2p3/cc/storage/treqs/hsm/hpssJNI/NativeBridge.java -encoding UTF8
 rm -f fr_*.h
 javah -classpath ./ -d ./ -jni fr.in2p3.cc.storage.treqs.hsm.hpssJNI.NativeBridge
 
 echo Compiling JNI Bridge - c
 rm -f NativeBridge.o
-gcc -I /opt/jdk1.6.0_18/include/linux/ -I /opt/hpss/include/ -I ./ -DLINUX -Wall -fPIC -o ./NativeBridge.o -c ../src/main/c/NativeBridge.c
+gcc -I /opt/jdk1.6.0_18/include/linux/ -I /opt/hpss/include/ -I ./ -DLINUX -Wall -fPIC -o ./NativeBridge.o -c ../native/src/main/c/NativeBridge.c
 
 rm -f libNativeBridge.so
 # It is necessary to include the authentication / authorization libraries in
--- java/src/test/resources/executeJNIBridgeJava.sh
+++ java/src/test/resources/executeJNIBridgeJava.sh
@@ -8,7 +8,7 @@ sh ./compileJNIBridgeJava.sh
 
 # HPSS JNI Bridge in Java.
 echo Compiling tester
-javac -cp . -d . ../src/test/java/fr/in2p3/cc/storage/treqs/hsm/hpssJNI/NativeBridgeTester.java
+javac -cp . -d . ../java/src/test/java/fr/in2p3/cc/storage/treqs/hsm/hpssJNI/NativeBridgeTester.java
 
 echo Executing
 # This is for HPSS logging (it works from 0 - 7, the three bits)
@@ -17,5 +17,5 @@ export HPSS_API_DEBUG=0
 export TREQS_LOG=WARN
 export LD_LIBRARY_PATH=`pwd`:/opt/hpss/lib/
 
-java -Djava.library.path=./ -ea -cp ./ fr.in2p3.cc.storage.treqs.hsm.hpssJNI.NativeBridgeTester
+java -Djava.library.path=`pwd`:/opt/hpss/lib/ -ea -cp ./ fr.in2p3.cc.storage.treqs.hsm.hpssJNI.NativeBridgeTester
 
