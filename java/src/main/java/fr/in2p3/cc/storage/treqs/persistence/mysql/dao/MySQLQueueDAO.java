f2deb6f848339dba46d92a920a9e35f30270f6ad
Andres Gomez Casanova <andres.gomez@cc.in2p3.fr>
2011-02-17 19:32:25 +0100

    More assertions when updating a queue in the database.

--- java/src/main/java/fr/in2p3/cc/storage/treqs/persistence/mysql/dao/MySQLQueueDAO.java
+++ java/src/main/java/fr/in2p3/cc/storage/treqs/persistence/mysql/dao/MySQLQueueDAO.java
@@ -229,6 +229,7 @@ public final class MySQLQueueDAO implements QueueDAO {
         try {
             switch (status) {
             case ACTIVATED:
+                assert nbDone == 0 && nbFailed == 0;
                 statement = MySQLBroker.getInstance().getPreparedStatement(
                         MySQLStatements.SQL_QUEUES_UPDATE_QUEUE_ACTIVATED);
                 // Insert activation time
@@ -236,11 +237,13 @@ public final class MySQLQueueDAO implements QueueDAO {
                         new Timestamp(time.getTimeInMillis()));
                 break;
             case CREATED:
+                assert nbDone == 0 && nbFailed == 0;
                 // This call could be done when the queue is unsuspended.
                 LOGGER.error("This is an invalid state call.");
                 assert false;
                 break;
             case ENDED:
+                assert nbDone != 0 || nbFailed != 0;
                 // This should be ENDED or ABORTED or
                 // TEMPORARILY_SUSPENDED
                 statement = MySQLBroker.getInstance().getPreparedStatement(
