247df0c6f7cb4720300f550b4e4df2e5034e8ba1
Andres Gomez <angoca@yahoo.com>
2011-03-15 00:13:39 -0500

    Tests a file in a big position

--- java/src/test/java/fr/in2p3/cc/storage/treqs/model/QueueTest.java
+++ java/src/test/java/fr/in2p3/cc/storage/treqs/model/QueueTest.java
@@ -1268,6 +1268,30 @@ public final class QueueTest {
     }
 
     /**
+     * Tests to register a file in a very big position. Bigger that a short.
+     *
+     * @throws TReqSException
+     *             Never.
+     */
+    @Test
+    public void testRegisterFile16BigPosition() throws TReqSException {
+        String filename = "testBig";
+        int position = 100000;
+        String tapename = "tapename";
+        MediaType mediaType = new MediaType((byte) 1, "mediaType");
+
+        Tape tape = new Tape(tapename, mediaType);
+        Queue queue = new Queue(new FilePositionOnTape(new File("filename",
+                QueueTest.HUNDRED), QueueTest.TEN, tape, new User("username")),
+                (byte) THREE);
+        User owner = new User("user");
+
+        FilePositionOnTape fpot1 = new FilePositionOnTape(new File(filename,
+                QueueTest.TEN), position, tape, owner);
+        queue.registerFPOT(fpot1, (byte) 1);
+    }
+
+    /**
      * Tries to set a negative creation time.
      *
      * @throws TReqSException
