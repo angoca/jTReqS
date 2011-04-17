2b6c68ef7b10b621a48a80141d8482ab55611757
Andres Gomez <angoca@yahoo.com>
2011-04-17 15:41:55 -0500

    New tables

--- doc/mysql.sql
+++ doc/mysql.sql
@@ -30,6 +30,19 @@ CREATE TABLE `heart_beat` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `information`
+--
+
+DROP TABLE IF EXISTS `information`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `information` (
+  `name` varchar(128) NOT NULL,
+  `value` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `jallocations`
 --
 
@@ -46,6 +59,16 @@ CREATE TABLE `jallocations` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `jallocations`
+--
+
+LOCK TABLES `jallocations` WRITE;
+/*!40000 ALTER TABLE `jallocations` DISABLE KEYS */;
+INSERT INTO `jallocations` VALUES (1,'user1','0.30'),(1,'user2','0.15'),(2,'user1','0.30'),(2,'user2','0.10'),(3,'user2','0.50');
+/*!40000 ALTER TABLE `jallocations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `jmediatypes`
 --
 
@@ -59,6 +82,16 @@ CREATE TABLE `jmediatypes` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jmediatypes`
+--
+
+LOCK TABLES `jmediatypes` WRITE;
+/*!40000 ALTER TABLE `jmediatypes` DISABLE KEYS */;
+INSERT INTO `jmediatypes` VALUES (1,'T10K-A',10),(2,'T10K-B',5),(3,'T10K-C',30);
+/*!40000 ALTER TABLE `jmediatypes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `jqueues`
@@ -84,7 +117,7 @@ CREATE TABLE `jqueues` (
   PRIMARY KEY (`id`),
   KEY `mediatype_id` (`mediatype_id`),
   CONSTRAINT `jqueues_ibfk_1` FOREIGN KEY (`mediatype_id`) REFERENCES `jmediatypes` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -117,10 +150,53 @@ CREATE TABLE `jrequests` (
   PRIMARY KEY (`id`),
   KEY `queue_id` (`queue_id`),
   CONSTRAINT `jrequests_ibfk_1` FOREIGN KEY (`queue_id`) REFERENCES `jqueues` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `queue_status`
+--
+
+DROP TABLE IF EXISTS `queue_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `queue_status` (
+  `id` int(11) DEFAULT NULL,
+  `name` char(21) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `queue_status`
+--
+
+LOCK TABLES `queue_status` WRITE;
+/*!40000 ALTER TABLE `queue_status` DISABLE KEYS */;
+INSERT INTO `queue_status` VALUES (240,'Aborted'),(210,'Activated'),(200,'Created'),(230,'Ended'),(220,'Temporarily Suspended');
+/*!40000 ALTER TABLE `queue_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `request_status`
+--
+
+DROP TABLE IF EXISTS `request_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `request_status` (
+  `id` int(11) DEFAULT NULL,
+  `name` char(10) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `request_status`
+--
+
+LOCK TABLES `request_status` WRITE;
+/*!40000 ALTER TABLE `request_status` DISABLE KEYS */;
+INSERT INTO `request_status` VALUES (100,'Created'),(160,'Failed'),(150,'On disk'),(120,'Queued'),(140,'Staged'),(110,'Submitted');
+/*!40000 ALTER TABLE `request_status` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -132,4 +208,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-03-31  0:28:01
+-- Dump completed on 2011-04-17 15:35:54
