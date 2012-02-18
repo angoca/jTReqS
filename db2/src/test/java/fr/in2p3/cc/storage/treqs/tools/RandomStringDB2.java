ca4b7666527fa290b9c1a4d18a0a455e3ea889ef
Andres Gomez Casanova <angoca@yahoo.com>
2012-02-18 11:11:48 -0500

    New files for db2 component
    extra files for execution
    Helper and name generator

---
+++ db2/src/test/java/fr/in2p3/cc/storage/treqs/tools/RandomStringDB2.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright      Jonathan Schaeffer 2009-2010,
+ *                  CC-IN2P3, CNRS <jonathan.schaeffer@cc.in2p3.fr>
+ * Contributors   Andres Gomez,
+ *                  CC-IN2P3, CNRS <andres.gomez@cc.in2p3.fr>
+ *
+ * This software is a computer program whose purpose is to schedule, sort
+ * and submit file requests to the hierarchical storage system HPSS.
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights, and the successive licensors have only limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ *
+ */
+package fr.in2p3.cc.storage.treqs.tools;
+
+import java.util.Random;
+
+/**
+ * Creates random strings.
+ *
+ * @author Andrés Gómez
+ * @since 1.5
+ */
+public final class RandomStringDB2 {
+
+    /**
+     * Array for the characters.
+     */
+    private static final char[] SYMBOLS = new char[36];
+
+    static {
+        for (int idx = 0; idx < 10; ++idx) {
+            SYMBOLS[idx] = (char) ('0' + idx);
+        }
+        for (int idx = 10; idx < 36; ++idx) {
+            SYMBOLS[idx] = (char) ('a' + idx - 10);
+        }
+    }
+
+    /**
+     * Array that contain the characters of the strings.
+     */
+    private final char[] buf;
+
+    /**
+     * Randomizer.
+     */
+    private final Random random = new Random();
+
+    /**
+     * Creates a string.
+     *
+     * @param length
+     *            Quantity of characters in the string.
+     */
+    public RandomStringDB2(final int length) {
+        if (length < 1) {
+            throw new IllegalArgumentException("length < 1: " + length);
+        }
+        this.buf = new char[length];
+    }
+
+    /**
+     * @return Returns a random string.
+     */
+    public String nextString() {
+        for (int idx = 0; idx < this.buf.length; ++idx) {
+            this.buf[idx] = SYMBOLS[this.random.nextInt(SYMBOLS.length)];
+        }
+        return new String(this.buf);
+    }
+
+}
